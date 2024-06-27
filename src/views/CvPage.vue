44d7aae27a416d2f9fd8c5bbfa8c3ce4fb621f05
Pedro Costa <163901231+Poettawds@users.noreply.github.com>
2024-06-27 21:28:22 +0200

    Fix filename case issue

--- src/views/CvPage.vue
+++ src/views/CvPage.vue
@@ -1,177 +1,431 @@
 <template>
+  <div class="container">
+    <div id="left-side">
+      <section class="personal-info">
+        <h3>Informations personnelles</h3>
+        <ul>
+          <li>Email:<a href="mailto:poettawds@gmail.com">poettawds@gmail.com</a></li>
+          <li>Téléphone:<a href="callto:0753812381">0753812381</a></li>
+          <li>Localisation: Annecy, 74000</li>
+          <li>Permis: B</li>
+        </ul>
+      </section>
 
-<!DOCTYPE html>
-<html lang="fr">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>CV - Pedro Costa</title>
-    <link rel="stylesheet" href="styles.css">
-</head>
-<body>
-    <div class="container">
-        <header>
-            <h1>Pedro Costa</h1>
-            <h2>Développeur full stack junior</h2>
-        </header>
-        <section>
-            <h3>Informations personnelles</h3>
-            <ul>
-                <li>Email: <a href="mailto:poettawds@gmail.com">poettawds@gmail.com</a></li>
-                <li>Téléphone: 0753812381</li>
-                <li>Localisation: Annecy, 74000</li>
-                <li>Permis: B</li>
-            </ul>
-        </section>
-        <section>
-            <h3>Mes liens</h3>
-            <ul>
-                <li><a href="https://linkedin.com/in/pedronfcosta/">LinkedIn</a></li>
-                <li><a href="https://github.com/Poetta12">GitHub</a></li>
-                <li><a href="http://www.poettatech.fr/">poettatech.fr</a></li>
-            </ul>
-        </section>
-        <section>
-            <h3>Profil</h3>
-            <p>Actuellement en formation de Développeur Avancé, je recherche une alternance de 12 mois. Dynamique et ayant un excellent sens du contact, j'aime collaborer avec des interlocuteurs variés. Volontaire, je me distingue par mon engagement au service du collectif et mon goût d'apprendre.</p>
-            <p>Ma vie est enrichie par le cinéma, l'œnologie, la musique, le football et la musculation, les activités associatives, le théâtre, ainsi que ma dévotion à la lecture.</p>
-        </section>
-        <section>
-            <h3>Expérience professionnelle significative</h3>
-            <ul>
-                <li><strong>Développeur front-end, CodePhenix, Île-de-France</strong> (janvier 2023 - mars 2024)
-                    <ul>
-                        <li>Utilisation des frameworks tels que Bootstrap et Vue.js pour développer des interfaces utilisateur modernes et réactives.</li>
-                        <li>Développement d'applications web en utilisant des langages de programmation tels que JavaScript, HTML et CSS</li>
-                    </ul>
-                </li>
-                <li><strong>Department Sales Manager, E.Leclerc, Île de France</strong> (septembre 2019 - décembre 2019)
-                    <ul>
-                        <li>Coaching des équipes commerciales et formation aux méthodes de vente pour améliorer les résultats.</li>
-                        <li>Management d'une équipe et participation au suivi administratif et RH dans le respect des procédures internes.</li>
-                    </ul>
-                </li>
-                <li><strong>Directeur magasin, Lidl France, Île de France</strong> (mars 2013 - juin 2017)
-                    <ul>
-                        <li>Gestion des stocks et suivi des commandes pour assurer une disponibilité optimale des produits.</li>
-                        <li>Management des équipes en veillant à la qualité du climat social.</li>
-                    </ul>
-                </li>
-            </ul>
-        </section>
-        <section>
-            <h3>Formation</h3>
-            <ul>
-                <li><strong>Dev Avancé, TP6</strong> (Campus Numérique in the Alps, avr. 2024 - janv. 2026)</li>
-                <li><strong>Licence - JAVA</strong> (Conservatoire National des Arts et Métiers, sept. 2023 - juin 2026)</li>
-                <li><strong>Licence - MUC</strong> (Universidade Nova de Lisboa, sept. 2020 - juin 2023)</li>
-            </ul>
-        </section>
-        <section>
-            <h3>Compétences</h3>
-            <ul>
-                <li>JavaScript</li>
-                <li>PHP</li>
-                <li>Java</li>
-                <li>Vue.js</li>
-                <li>Laravel</li>
-                <li>Développement agile</li>
-            </ul>
-        </section>
-        <section>
-            <h3>Langues</h3>
-            <ul>
-                <li>Portugais</li>
-                <li>Français</li>
-                <li>Anglais</li>
-                <li>Espagnol</li>
-            </ul>
-        </section>
-        <section>
-            <h3>Références</h3>
-            <ul>
-                <li>Brieuc Lebars, Code Phenix (brieuc@codephenix.com)</li>
-                <li>Aliou Diaby, Formateur indépendant Développeur Web (diabyaliou19@gmail.com)</li>
-                <li>Philippe Strapazzon, Campus Numérique in the Alps (philippe.strappazzon@le-campus-numerique.fr)</li>
-            </ul>
-        </section>
-        <section>
-            <h3>Centres d'intérêt</h3>
-            <p>Cinéma, œnologie, musique, football et musculation, activités associatives, théâtre, lecture.</p>
-        </section>
+      <section class="links">
+        <h3>Mes liens</h3>
+        <ul>
+          <li class="icon-github"><a href="https://linkedin.com/in/pedronfcosta/">LinkedIn</a></li>
+          <li class="icon-linkedin"><a href="https://github.com/Poetta12">GitHub</a></li>
+          <li class="icon-x"><a href="https://x.com/poettawds">X / Twitter</a></li>
+          <li class="icon-profile"><a href="http://www.poettatech.fr/">poettatech.fr</a></li>
+        </ul>
+      </section>
+
+      <section class="education">
+        <h3>Formation</h3>
+        <ul>
+          <li>
+            <strong>Dev Avancé, TP6</strong> (Campus Numérique in the Alps, avr. 2024 - janv. 2026)
+          </li>
+          <li>
+            <strong>Licence - JAVA</strong> (Conservatoire National des Arts et Métiers, sept. 2023
+            - juin 2026)
+          </li>
+          <li>
+            <strong>Licence - MUC</strong> (Universidade Nova de Lisboa, sept. 2020 - juin 2023)
+          </li>
+        </ul>
+      </section>
+
+      <section class="skills">
+        <h3>Compétences</h3>
+        <ul>
+          <li class="tooltip">
+            <img src="/src/assets/logos/html.webp" alt="logo html" /><span class="tooltiptext"
+              >HTML</span
+            >
+          </li>
+          <li class="tooltip">
+            <img src="/src/assets/logos/CSS.png" alt="logo css" /><span class="tooltiptext"
+              >CSS</span
+            >
+          </li>
+          <li class="tooltip">
+            <img src="/src/assets/logos/js.png" alt="logo JavaScript" /><span class="tooltiptext"
+              >JavaScript</span
+            >
+          </li>
+          <li class="tooltip">
+            <img src="/src/assets/logos/php.png" alt="logo PHP" /><span class="tooltiptext"
+              >PHP</span
+            >
+          </li>
+          <li class="tooltip">
+            <img src="/src/assets/logos/java.png" alt="logo Java" /><span class="tooltiptext"
+              >Java</span
+            >
+          </li>
+          <li class="tooltip">
+            <img src="/src/assets/logos/sql.webp" alt="logo sql" /><span class="tooltiptext"
+              >SQL</span
+            >
+          </li>
+          <li class="tooltip">
+            <img src="/src/assets/logos/Group-59.webp" alt="logo agile" /><span class="tooltiptext"
+              >Développement Agile</span
+            >
+          </li>
+          <li class="tooltip">
+            <img src="/src/assets/logos/agile_logo.png" alt="logo scrum" /><span class="tooltiptext"
+              >Méthodologie Scrum</span
+            >
+          </li>
+          <li class="tooltip">
+            <img src="/src/assets/logos/logo.svg" alt="logo vue" /><span class="tooltiptext"
+              >Vue.js</span
+            >
+          </li>
+          <li class="tooltip">
+            <img src="/src/assets/logos/laravel.jpg" alt="logo laravel" /><span class="tooltiptext"
+              >Laravel</span
+            >
+          </li>
+          <li class="tooltip">
+            <img src="/src/assets/logos/SYM.png" alt="logo Symphony" /><span class="tooltiptext"
+              >Symphony</span
+            >
+          </li>
+          <li class="tooltip">
+            <img src="/src/assets/logos/wp.jpg" alt="logo wordpress" /><span class="tooltiptext"
+              >WordPress</span
+            >
+          </li>
+        </ul>
+      </section>
+
+      <section class="languages">
+        <h3>Langues</h3>
+        <ul>
+          <li class="tooltip">
+            <img src="/src/assets/logos/pt.jpg" alt="Portugal" /><span class="tooltiptext"
+              >Portugal</span
+            >
+          </li>
+          <li class="tooltip">
+            <img src="/src/assets/logos/fr.jpg" alt="France" /><span class="tooltiptext"
+              >France</span
+            >
+          </li>
+          <li class="tooltip">
+            <img src="/src/assets/logos/en1.png" alt="Anglais" /><span class="tooltiptext"
+              >English</span
+            >
+          </li>
+          <li class="tooltip">
+            <img src="/src/assets/logos/spain.jpg" alt="Espagnol" /><span class="tooltiptext"
+              >Español</span
+            >
+          </li>
+        </ul>
+      </section>
     </div>
-</body>
-</html>
+    <div id="right-side">
+      <header>
+        <h1>Pedro Costa</h1>
+        <h2>Développeur full stack junior</h2>
+      </header>
+
+      <section class="profile">
+        <h3>Profil</h3>
+        <p>
+          Actuellement en formation de Développeur Avancé, je recherche une alternance de 12 mois.
+          Dynamique et ayant un excellent sens du contact, j'aime collaborer avec des interlocuteurs
+          variés. Volontaire, je me distingue par mon engagement au service du collectif et mon goût
+          d'apprendre.
+        </p>
+      </section>
+      <section class="experience">
+        <h3>Expérience professionnelle significative</h3>
+        <ul>
+          <li>
+            <strong>Développeur front-end, CodePhenix, Île-de-France</strong> (janvier 2023 - mars
+            2024)
+            <ul>
+              <li>
+                Utilisation des frameworks tels que Bootstrap et Vue.js pour développer des
+                interfaces utilisateur modernes et réactives.
+              </li>
+              <li>
+                Développement d'applications web en utilisant des langages de programmation tels que
+                JavaScript, HTML et CSS
+              </li>
+            </ul>
+          </li>
+          <li>
+            <strong>Department Sales Manager, E.Leclerc, Île de France</strong> (septembre 2019 -
+            décembre 2019)
+            <ul>
+              <li>
+                Coaching des équipes commerciales et formation aux méthodes de vente pour améliorer
+                les résultats.
+              </li>
+              <li>
+                Management d'une équipe et participation au suivi administratif et RH dans le
+                respect des procédures internes.
+              </li>
+            </ul>
+          </li>
+          <li>
+            <strong>Directeur magasin, Lidl France, Île de France</strong> (mars 2013 - juin 2017)
+            <ul>
+              <li>
+                Gestion des stocks et suivi des commandes pour assurer une disponibilité optimale
+                des produits.
+              </li>
+              <li>Management des équipes en veillant à la qualité du climat social.</li>
+            </ul>
+          </li>
+        </ul>
+      </section>
+
+      <section class="interests">
+        <h3>Centres d'intérêt</h3>
+        <p>
+          Cinéma, œnologie, musique, football et musculation, activités associatives, théâtre,
+          lecture.
+        </p>
+      </section>
+
+      <section class="references">
+        <h3>Références</h3>
+        <ul>
+          <li>Brieuc Lebars, Code Phenix (brieuc@codephenix.com)</li>
+          <li>Aliou Diaby, Formateur indépendant Développeur Web (diabyaliou19@gmail.com)</li>
+          <li>
+            Philippe Strapazzon, Campus Numérique in the Alps
+            (philippe.strappazzon@le-campus-numerique.fr)
+          </li>
+        </ul>
+      </section>
+    </div>
+  </div>
 </template>
 
-<script setup></script>
+<script setup>
+import jsLogo from '@/assets/logos/js.png' // Utilisation d'un chemin d'importation absolu avec Vite
+</script>
 
 <style scoped>
+/* Assurez-vous d'inclure ce reset au début de votre fichier CSS */
+*,
+*::before,
+*::after {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+
 body {
-    font-family: Arial, sans-serif;
-    line-height: 1.6;
-    margin: 0;
-    padding: 0;
-    background-color: #f4f4f4;
+  font-family: Arial, sans-serif;
+  background-color: #f4f4f9;
+  color: #333;
 }
 
 .container {
-    max-width: 800px;
-    margin: 20px auto;
-    padding: 20px;
-    background: #fff;
-    border-radius: 8px;
-    box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+  display: flex;
+  flex-wrap: wrap; /* Permet de passer en colonne sur les petits écrans */
+  width: 100%;
+  max-width: 900px;
+  margin: 20px auto;
+  background: #fff;
+  border-radius: 8px;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+  overflow: hidden; /* Empêche le débordement */
+}
+
+#left-side,
+#right-side {
+  padding: 20px;
+  text-align: left; /* Aligner le texte à gauche */
+}
+
+#left-side {
+  width: 35%;
+  background: #333;
+  color: #fff;
+  order: 1; /* Initialement avant le right side */
+}
+
+#left-side h3 {
+  color: rgb(253, 193, 123);
+}
+
+#left-side ul {
+  list-style: none;
+}
+
+#left-side ul li a {
+  padding-left: 10px;
+  color: #a2cbf5;
+  text-decoration: none;
+  text-align: left; /* Aligner les liens à gauche */
+}
+
+#left-side ul li a:hover {
+  text-decoration: underline;
+}
+
+#right-side {
+  width: 65%;
+  background: #fff;
+  order: 2; /* Initialement après le left side */
 }
 
 header {
-    text-align: center;
-    margin-bottom: 20px;
+  background: rgb(253, 193, 123);
+  padding: 20px;
+  border-radius: 0 100px 100px 0;
+  margin-bottom: 20px;
+  text-align: left; /* Aligner le texte du header à gauche */
 }
 
 header h1 {
-    margin: 0;
-    font-size: 2em;
-    color: #333;
+  font-size: 2.5em;
+  margin-bottom: 10px;
+  color: #333;
+  text-align: left; /* Aligner le texte à gauche */
 }
 
 header h2 {
-    margin: 0;
-    font-size: 1.5em;
-    color: #666;
+  font-size: 1.5em;
+  color: #666;
+  text-align: left; /* Aligner le texte à gauche */
 }
 
 section {
-    margin-bottom: 20px;
+  margin-bottom: 20px;
+  text-align: left; /* Aligner le texte à gauche */
 }
 
 section h3 {
-    margin-bottom: 10px;
-    font-size: 1.2em;
-    color: #333;
+  font-size: 1.4em;
+  color: #333;
+  border-bottom: 2px solid #666;
+  padding-bottom: 5px;
+  margin-bottom: 10px;
+  text-align: left; /* Aligner le texte à gauche */
 }
 
-ul {
-    list-style: none;
-    padding: 0;
+.skills ul,
+.languages ul {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-evenly;
+  text-align: left; /* Aligner le texte à gauche */
 }
 
-ul li {
-    margin-bottom: 10px;
+.skills img,
+.languages img {
+  width: 64px;
+  height: 64px;
+  background: #fff;
 }
 
-ul li a {
-    color: #0066cc;
-    text-decoration: none;
+.tooltip {
+  position: relative;
+  display: inline-block;
 }
 
-ul li a:hover {
-    text-decoration: underline;
+.tooltip .tooltiptext {
+  visibility: hidden;
+  width: 150px;
+  background-color: rgb(253, 193, 123);
+  color: #333;
+  text-align: center;
+  border-radius: 5px;
+  padding: 5px 0;
+  position: absolute;
+  z-index: 1;
+  bottom: 125%; /* Positionne le tooltip au-dessus de l'élément */
+  left: 50%;
+  margin-left: -75px; /* Centre le tooltip */
+  opacity: 0;
+  transition: opacity 0.3s;
 }
 
-p {
-    margin: 0 0 10px;
+.tooltip .tooltiptext::after {
+  content: '';
+  position: absolute;
+  top: 100%; /* En bas du tooltip */
+  left: 50%;
+  margin-left: -5px;
+  border-width: 5px;
+  border-style: solid;
+  border-color: rgb(253, 193, 123) transparent transparent transparent;
 }
 
+.tooltip:hover .tooltiptext {
+  visibility: visible;
+  opacity: 1;
+}
 
+.links ul li {
+  line-height: 1.7;
+}
+
+@media (max-width: 768px) {
+  body {
+    overflow-x: hidden; /* Empêche le défilement horizontal lorsque le menu est ouvert */
+  }
+
+  .container {
+    flex-direction: column;
+  }
+
+  #left-side,
+  #right-side {
+    width: 100%; /* Faire en sorte que les deux côtés prennent 100% de la largeur en mode mobile */
+  }
+  #right-side {
+    order: 1; /* right-side avant left-side en mode mobile */
+  }
+  #left-side {
+    order: 2; /* left-side après right-side en mode mobile */
+  }
+
+  .main-nav {
+    position: fixed;
+    top: 0;
+    left: 0;
+    height: 100vh;
+    width: 100%;
+    background-color: rgba(0, 0, 0, 0.7);
+    flex-direction: column;
+    place-content: start;
+    align-items: flex-end;
+    z-index: 999;
+    transform: translateX(-100%);
+    transition: transform 0.3s ease-in-out;
+    overflow: hidden;
+  }
+
+  .main-nav.show {
+    transform: translateX(0);
+  }
+
+  .main-nav ul {
+    text-align: right;
+    flex-direction: column;
+    margin-top: 5rem;
+    padding: 0.3rem;
+    background: rgba(0, 0, 0, 0.7);
+    border-radius: 10px 0 0 10px;
+    width: 100%; /* Ajuste la largeur du menu à 100% */
+  }
+
+  header {
+    border-radius: 0;
+    text-align: center;
+  }
+}
 </style>
